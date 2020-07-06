cf0f02a3815d567039803052ce7e295cb6139d2b
Jonny <e.yarshin@gmail.com>
2020-07-06 12:42:34 +0300

    Customize users fragment - add lines

--- app/src/main/java/com/example/testtask/ui/users/UsersFragment.java
+++ app/src/main/java/com/example/testtask/ui/users/UsersFragment.java
@@ -2,10 +2,16 @@ package com.example.testtask.ui.users;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,7 +28,10 @@ import java.util.Objects;
 
 public class UsersFragment extends AbstractFragment {
 
+    private ImageView drawingImageView;
+
     private UsersViewModel mUsersViewModel;
+
     private UsersAdapter.OnItemClickListener mOnItemClickListener = id -> {
         Intent intent = new Intent(getActivity(), PhotosActivity.class);
         Bundle args = new Bundle();
@@ -50,5 +59,49 @@ public class UsersFragment extends AbstractFragment {
         binding.setVm(mUsersViewModel);
         binding.setLifecycleOwner(this);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        drawingImageView = Objects.requireNonNull(getView()).findViewById(R.id.users_background);
+        view.post(this::drawingLines);
+    }
+
+    private void drawingLines() {
+
+        WindowManager windowManager = Objects.requireNonNull(getActivity()).getWindowManager();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displaymetrics);
+
+        float dpi = displaymetrics.density;
+
+        int ScreenStep;
+        if(dpi > 1.5)
+            ScreenStep = (int)(34.9 * displaymetrics.density);
+        else if(dpi > 0.75)
+            ScreenStep = (int)(35.9 * displaymetrics.density);
+        else
+            ScreenStep = (int)(36 * displaymetrics.density);
+
+
+        int screenWidth = drawingImageView.getWidth();
+        int screenHeight = drawingImageView.getHeight();
+        Bitmap bitmap = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawingImageView.setImageBitmap(bitmap);
+
+        Paint paint = new Paint();
+
+        paint.setColor(getResources().getColor(R.color.colorGray));
+
+        int strokeWidth = (int)(1 * displaymetrics.density);
+        if(strokeWidth == 0)
+            strokeWidth = 1;
+
+        paint.setStrokeWidth(strokeWidth);
+
+        for (int i = ScreenStep; i <= screenHeight; i += ScreenStep)
+            canvas.drawLine(0, i, screenWidth, i, paint);
     }
 }
